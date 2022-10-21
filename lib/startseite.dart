0a3993656681bbfa12ba98160969adff888efa65
Rabia <rabia.nur.tunen@ogrenci.karatay.edu.tr>
2022-10-21 13:48:13 +0300

    drawer

--- lib/startseite.dart
+++ lib/startseite.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class StartSeite extends StatefulWidget {
@@ -12,6 +14,45 @@ class _StartSeiteState extends State<StartSeite> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const ImageIcon(
+            AssetImage('assets/images/menu.png'),
+            color: Colors.white,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: const Color(0XFFA3333D),
+        width: 300,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.horizontal(
+              left: Radius.zero, right: Radius.circular(32.0)),
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.fromLTRB(36.0, 54.0, 0.0, 36.0),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'NOTELY',
+                style: TextStyle(
+                  fontFamily: 'Titan One',
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                  color: Color(0XFFFFFDFA),
+                ),
+              ),
+            ),
+            Container()
+          ],
+        ),
+      ),
     );
   }
 }
