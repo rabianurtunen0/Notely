0867edf164f560d460f10c96d5389c0c07bc2346
Rabia <rabia.nur.tunen@ogrenci.karatay.edu.tr>
2022-12-07 00:28:17 +0300

    delete highlight color

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 
 class Themes {
+
   static final lightTheme = ThemeData(
     fontFamily: 'Nunito',
     backgroundColor: const Color(0XFFEAEAEA),
-    highlightColor: const Color(0XFFA3333D),
+    shadowColor: Colors.white,
     textSelectionTheme: const TextSelectionThemeData(
       selectionColor: Color(0XFF2A2B2E),
-      selectionHandleColor: Color(0XFF2A2B2E)
+      selectionHandleColor: Color(0XFF2A2B2E),
     ),
-    shadowColor: Colors.white, 
   );
 
   static final darkTheme = ThemeData(
     fontFamily: 'Nunito',
     backgroundColor: const Color(0XFF2A2B2E),
-    highlightColor: const Color(0XFFA3333D),
+    shadowColor: Colors.black,
     textSelectionTheme: const TextSelectionThemeData(
       selectionColor: Color(0XFFEAEAEA),
       selectionHandleColor: Color(0XFFD9D9D9),
     ),
-    shadowColor: Colors.black,
   );
 
 }
